b654b08776e24b5edcab3d2acf5c2faa731bbc6d
Mikhail Kabanov <mike.k@didlogic.com>
2023-12-03 19:54:35 +0600

    Add benchmark

--- 2023/day3/main_test.go
+++ 2023/day3/main_test.go
@@ -20,3 +20,18 @@ func TestPart2(t *testing.T) {
 	schematic := loadSchematic("input.txt")
 	assert.Equal(t, Part2(schematic), 80694070)
 }
+
+func Benchmark_Part1(b *testing.B) {
+
+	for n := 0; n < b.N; n++ {
+		schematic := loadSchematic("input.txt")
+		Part1(schematic)
+	}
+}
+
+func Benchmark_Part2(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		schematic := loadSchematic("input.txt")
+		Part2(schematic)
+	}
+}
